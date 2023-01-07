86d0cffcec79c128cd234495c77c05ce8538dfa4
touseef-haider <touseefhaider355@gmail.com>
2023-01-07 16:28:07 +0500

    chore: added mosques

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Pointer from "../../assets/point.png";
 import GoogleMapReact from "google-map-react";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => (
+  <div>
+    <img src={Pointer} height={50} width={50} alt="pointer" />
+  </div>
+);
 
 export default function MapComponent() {
   const defaultProps = {
@@ -9,8 +14,31 @@ export default function MapComponent() {
       lat: 10.99835602,
       lng: 77.01502627,
     },
-    zoom: 11,
+    zoom: 20,
   };
+
+  const mosques = [
+    {
+      lat: 30.404955,
+      lng: 70.736504,
+    },
+    {
+      lat: 32.959686,
+      lng: 73.689919,
+    },
+    {
+      lat: 26.05702,
+      lng: 68.846275,
+    },
+    {
+      lat: 24.907162,
+      lng: 67.058723,
+    },
+    {
+      lat: 30.894875,
+      lng: 72.652359,
+    },
+  ];
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
@@ -19,7 +47,9 @@ export default function MapComponent() {
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        {mosques.map(({ lat, lng }) => (
+          <AnyReactComponent lat={lat} lng={lng} text="My Marker" />
+        ))}
       </GoogleMapReact>
     </div>
   );
