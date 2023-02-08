b95c04fb6d47eb3a6c1f0852055f2e8504180944
touseef-haider <touseefhaider355@gmail.com>
2023-02-08 09:56:19 +0500

    chore: done with the provice error

--- src/pages/placesNearMe/index.js
+++ src/pages/placesNearMe/index.js
@@ -85,9 +85,7 @@ const PlacesNearMe = () => {
             value={province}
             selectOption="select province"
             options={
-              Array.isArray(provinces) &&
-              province?.length > 0 &&
-              provinces?.map((p) => ({ value: p?._id, item: p?.name }))
+              provinces?.map((p) => ({ value: p?._id, item: p?.name })) || []
             }
           />
           {province && (
